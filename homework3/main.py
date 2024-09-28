d8d10d5e29fcded8fb48b78a5f45409f8d5bc2ed
LapTop_Bogdan <bogdanvoinov90@gmail.com>
2024-09-28 21:05:18 +0300

    add homework3 all

--- homework3/main.py
+++ homework3/main.py
@@ -1,17 +1,20 @@
 import re
+import json
 
 
-def ui(inp: str):
-    if inp[-1] != ";":
-        raise Exception("err")
+def parser(inp: str):
+    if inp[-1] != ";" or inp.count(";") != 1 or inp.count(' := ') != 1:
+        raise Exception("error")
+    inp = inp[:-1]
     inp = inp.split(' := ')
     inp[0] = inp[0].strip()
-    if inp[1].strip()[0] == '$':
+    if re.match(r'\$"[a-z_0-9A-Z`!#%^&*()]*"', inp[1].strip()):
         return inp[0], inp[1][2:-1]
     if inp[1].strip() == "begin":
         return inp[0], {}
     if re.search("[0-9]+", inp[1].strip()):
         return inp[0], int(inp[1])
+    raise Exception("error")
 
 
 def main():
@@ -19,21 +22,25 @@ def main():
     root = all_
     while True:
         line = input()
+        match = re.match(r"\[[_a-zA-Z]+\]", line)
         if line == '__stop__':
             break
-        if line == 'end':
+        elif line == 'end':
             root = all_
+        elif match and match.endpos == len(line):
+            if line[1:-1] in root.keys():
+                print(root[line[1:-1]])
+            else:
+                print("error not in memory")
         else:
             try:
-                key, var = ui(line)
-                if var is None:
-                    print(root[key])
+                key, var = parser(line)
                 root[key] = var
                 if var == {}:
                     root = root[key]
             except Exception as e:
                 print(e)
-    print(all_)
+    print(json.dumps(root))
 
 
 if __name__ == '__main__':
